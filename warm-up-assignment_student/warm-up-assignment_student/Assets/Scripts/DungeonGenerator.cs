3cc7401b37fa69e9fa91bcffac725addd9267da1
Daniël Groeneweg <daniel.groeneweg03@gmail.com>
2025-02-26 13:11:32 +0100

    random remove function, improved room splitting, bug fixing
    - Added a function to remove a random amount of random rooms
    - Improved the room splitting so that it splits rooms into random sizes rather than cutting the room in half
    - Fixed a bug that caused rooms to split below the set minimum size
    - Fixed a bug that caused all rooms to be generated at y=0

--- warm-up-assignment_student/warm-up-assignment_student/Assets/Scripts/DungeonGenerator.cs
+++ warm-up-assignment_student/warm-up-assignment_student/Assets/Scripts/DungeonGenerator.cs
@@ -1,88 +1,142 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 using NaughtyAttributes;
 
 public class DungeonGenerator : MonoBehaviour
 {
+    #region variables
     [Header("Dungeon Stats")]
     [SerializeField] private int dungeonWidth = 1000;
     [SerializeField] private int dungeonLength = 1000;
 
     [Header("Room Stats")]
     [SerializeField] private int roomMinWidth = 100;
-    [SerializeField] private int roomMinLength = 100;
-    [SerializeField] private int roomHeight = 50;
-    [SerializeField] [Range(0f, 1f)] private float ChanceToSplitRoom;
+    [SerializeField] private int roomMinHeight = 100;
 
     [Header("Generation Stats")]
+    [SerializeField] private int minimumAmountOfRooms = 10;
     [SerializeField] private int splitAmount = 10;
+    [SerializeField][Range(0f, 1f)] private float ChanceToSplitRoom = 0.5f;
+    [SerializeField] private int minRoomsToBeRemoved = 5;
+    [SerializeField] private int maxRoomsToBeRemoved = 10;
 
-    private RectInt dungeon;
     [Header("Rooms")]
     [SerializeField] private List<RectInt> rooms;
+
+    // Not in inspector
+    private RectInt dungeon;
+
+    private List<RectInt> removedRooms;
+    #endregion
+    private void Start()
+    {
+        dungeon = new RectInt(0, 0, dungeonWidth, dungeonLength);
+        rooms = new List<RectInt>();
+        removedRooms = new List<RectInt>();
+    }
+    #region RoomSplitting
     private void SplitRooms()
     {
+        // go through the list of rooms splitAmount times
         for (int loop = 1; loop <= splitAmount; loop++)
         {
             for (int i = rooms.Count - 1; i >= 0; i--)
             {
-                if (Random.Range(0f, 1f) <= ChanceToSplitRoom)
+                // there is a ChanceToSplitRoom chance that a room is going to be split, unless the minimum amount of rooms has not been reached yet
+                if (Random.Range(0f, 1f) <= ChanceToSplitRoom || rooms.Count < minimumAmountOfRooms)
                 {
-                    Debug.Log("Splitting!");
-
                     // Choose how to split
                     if (Random.Range(0, 2) == 1)
                     {
                         // Split horizontally if the room will not become too small, otherwise try vertically
-                        if (rooms[i].height / 2 > roomMinLength) SplitHorizontally(rooms[i]);
+                        if (rooms[i].height / 2 > roomMinHeight) SplitHorizontally(rooms[i]);
 
                         else if (rooms[i].width / 2 > roomMinWidth) SplitVertically(rooms[i]);
                     }
                     else
                     {
                         // Split vertically if the room will not become too small, otherwise try horizontally
-                        if (rooms[i].width > roomMinWidth) SplitVertically(rooms[i]);
+                        if (rooms[i].width / 2 > roomMinWidth) SplitVertically(rooms[i]);
 
-                        else if (rooms[i].height / 2 > roomMinLength) SplitHorizontally(rooms[i]);
+                        else if (rooms[i].height / 2 > roomMinHeight) SplitHorizontally(rooms[i]);
                     }
                 }
-                else Debug.Log("Did not split!");
             }
         }
     }
+    // Split room horizontally (reduce the y/height)
     private void SplitHorizontally(RectInt room)
     {
-        RectInt roomTop = new RectInt(room.x, room.y + room.height / 2, room.width, room.height / 2);
-        RectInt roomBottom = new RectInt(room.x, room.y, room.width, room.height / 2);
+        int randomSize = Random.Range(roomMinHeight, room.height - roomMinHeight + 1);
+        int restSize = room.height - randomSize;
+
+        RectInt roomTop = new RectInt(room.x, room.y + randomSize, room.width, restSize);
+        RectInt roomBottom = new RectInt(room.x, room.y, room.width, randomSize);
 
         rooms.Add(roomTop);
         rooms.Add(roomBottom);
 
         rooms.Remove(room);
+
+        Debug.Log("Room Height " + room.height + " Random Size " + randomSize + " Rest Size " + restSize);
     }
+    // Split room vertically (reduce the x/width)
     private void SplitVertically(RectInt room)
     {
-        RectInt roomLeft = new RectInt(room.x, room.y, room.width / 2, room.height);
-        RectInt roomRight = new RectInt(room.x + room.width / 2, room.y, room.width / 2, room.height);
+        int randomSize = Random.Range(roomMinWidth, room.width - roomMinWidth + 1);
+        int restSize = room.width - randomSize;
+
+        RectInt roomLeft = new RectInt(room.x, room.y, randomSize, room.height);
+        RectInt roomRight = new RectInt(room.x + randomSize, room.y, restSize, room.height);
 
         rooms.Add(roomLeft);
         rooms.Add(roomRight);
 
         rooms.Remove(room);
+
+        Debug.Log("Room Width " + room.width + " Random Size " + randomSize + " Rest Size " + restSize);
     }
+    #endregion
+
+    #region RoomRemoving
+    private void RemoveRandomRooms()
+    {
+        // Pick a random amount of rooms to be removed that lies between the minimum to be removed and maximum to be removed
+        int toBeDestroyed = Random.Range(minRoomsToBeRemoved, maxRoomsToBeRemoved + 1);
+        for (int i = 1; i <= toBeDestroyed; i++)
+        {
+            // Make sure there is always at least 1 room
+            if (rooms.Count > 1)
+            {
+                // Pick a random room and remove it
+                int index = Random.Range(0, rooms.Count);
+                removedRooms.Add(rooms[index]);
+                rooms.RemoveAt(index);
+            }
+        }
+    }
+    #endregion
+
+    #region DungeonDrawing
     void Update()
     {
+        // Draw existing rooms in yellow
         foreach (RectInt room in rooms)
         {
             AlgorithmsUtils.DebugRectInt(room, Color.yellow);
         }
 
-        AlgorithmsUtils.DebugRectInt(dungeon, Color.red);
-    }
+        // Draw dungeon in green
+        AlgorithmsUtils.DebugRectInt(dungeon, Color.green);
 
+        // Draw removed rooms in red
+        foreach (RectInt room in removedRooms)
+        {
+            AlgorithmsUtils.DebugRectInt(room, Color.red);
+        }
+    }
     [Button(enabledMode: EButtonEnableMode.Playmode)]
     public void GenerateDungeon()
     {
@@ -91,9 +145,12 @@ public class DungeonGenerator : MonoBehaviour
 
         // Reset dungeon
         rooms = new List<RectInt>();
+        removedRooms = new List<RectInt>();
 
         // Create new dungeon
         rooms.Add(new RectInt(0, 0, dungeonWidth, dungeonLength));
         SplitRooms();
+        RemoveRandomRooms();
     }
+    #endregion
 }
