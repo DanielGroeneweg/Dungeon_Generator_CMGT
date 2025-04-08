3bd2001d0177cff9bcf35d470e6b7846f2a2ac6d
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2025-04-08 14:07:23 +0200

    started creating player point and click functionality

--- Algorithms/Assets/Scripts/PlayerController.cs
+++ Algorithms/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Vector3 clickPosition;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get the mouse click position in world space 
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(mouseRay, out RaycastHit hitInfo))
+            {
+                Vector3 clickWorldPosition = hitInfo.point;
+                Debug.Log(clickWorldPosition);
+
+                // Store the click position here
+                clickPosition = clickWorldPosition;
+                // Trigger an unity event to notify other scripts about the click here
+
+            }
+        }
+
+        // Add visual debugging here
+        Debug.DrawLine(Camera.main.transform.position, clickPosition);
     }
 }
