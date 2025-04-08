54b7746814daee1b0b1ebea581dcba757424a1f7
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2025-04-08 12:48:32 +0200

    changed asset spawning to avoid duplicate objects

--- Algorithms/Assets/Scripts/DungeonVisualizing.cs
+++ Algorithms/Assets/Scripts/DungeonVisualizing.cs
@@ -13,18 +13,22 @@ public class DungeonVisualizing : MonoBehaviour
     [Header("Stats")]
     [SerializeField] private float wallHeight = 4;
 
+    [Header("Hierarchy")]
+    [SerializeField] private Transform roomsParentObject;
+
     private Graph<DungeonGenerator.Room> rooms;
     private Graph<DungeonGenerator.Room> doors;
 
-    // bools for visualization
-    bool madeFloor = false;
-
     private float TimeBetweenSteps = 0;
+
+    private HashSet<Vector2> discoveredPositions;
+    private void Start()
+    {
+        discoveredPositions= new HashSet<Vector2>();
+    }
     public void MakeDungeonPhysical(Graph<DungeonGenerator.Room> roomGraph, Graph<DungeonGenerator.Room> doorGraph, float time)
     {
         TimeBetweenSteps = time;
-
-        madeFloor = false;
 
         rooms = roomGraph;
         doors = doorGraph;
@@ -34,98 +38,79 @@ public class DungeonVisualizing : MonoBehaviour
     public void ClearDungeon()
     {
         StopAllCoroutines();
+        discoveredPositions.Clear();
 
-        Transform[] array = GetComponentsInChildren<Transform>();
-        List<Transform> list = new List<Transform>();
-
-        foreach(Transform obj in array)
+        // Remove all assets
+        foreach (Transform child in roomsParentObject)
         {
-            list.Add(obj);
-        }
-
-        for (int i = list.Count - 1; i >= 0; i--)
-        {
-            Transform obj = list[i];
-            
-            // Do not destroy this object
-            if (obj != transform)
-            {
-                Destroy(obj.gameObject);
-                list.Remove(obj);
-            }
+            Destroy(child.gameObject);
         }
     }
     private IEnumerator Generate()
     {
-        StartCoroutine(GenerateFloor());
-        yield return new WaitUntil(() => madeFloor);
-
-        GenerateWalls();
-    }
-    private IEnumerator GenerateFloor()
-    {
-        foreach (DungeonGenerator.Room room in rooms.KeysToList())
+        // Save door positions
+        foreach (DungeonGenerator.Room door in doors.KeysToList())
         {
-            Transform obj = GameObject.Instantiate(
-                floorPrefab,
-                new Vector3(room.area.x + room.area.width / 2f, 0, room.area.y + room.area.height / 2f),
-                Quaternion.identity, transform
-                );
-
-            obj.localScale = new Vector3(room.area.width, 0.1f, room.area.height);
-
-            yield return new WaitForSeconds(TimeBetweenSteps);
+            foreach(Vector2 position in door.area.allPositionsWithin)
+            {
+                discoveredPositions.Add(position);
+            }
         }
-        madeFloor = true;
-    }
-    private void GenerateWalls()
-    {
+
+        // Room generating
         foreach(DungeonGenerator.Room room in rooms.KeysToList())
         {
-            StartCoroutine(MakeWalls(room));
+            // Create parent object for this room
+            GameObject parentObject = new GameObject("Room_" + room.area.xMin + "_" + room.area.yMin + "_" + room.area.xMax + "_" + room.area.yMax);
+            parentObject.transform.parent = roomsParentObject;
+
+            // Generate floor for this room
+            GenerateFloor(room, parentObject);
+
+            // Generate walls for this room
+            StartCoroutine(MakeWalls(room, parentObject));
+
+            // Coroutine
+            yield return new WaitForSeconds(TimeBetweenSteps);
         }
+
+        // Spawn player in the first room of the list
+
     }
-    private IEnumerator MakeWalls(DungeonGenerator.Room room)
+    private void GenerateFloor(DungeonGenerator.Room room, GameObject parentObject)
+    {
+        Transform obj = GameObject.Instantiate(
+            floorPrefab,
+            new Vector3(room.area.x + room.area.width / 2f, 0, room.area.y + room.area.height / 2f),
+            Quaternion.identity,
+            parentObject.transform
+            );
+
+        obj.name = "Floor";
+        obj.localScale = new Vector3(room.area.width, 0.1f, room.area.height);
+    }
+    private IEnumerator MakeWalls(DungeonGenerator.Room room, GameObject parentObject)
     {
         foreach (Vector2 position in room.area.allPositionsWithin)
         {
             if (position.x == room.area.xMin || position.x == room.area.xMax - 1 || position.y == room.area.yMin || position.y == room.area.yMax - 1)
             {
-                bool pointIsIndoor = false;
+                if (discoveredPositions.Contains(position)) continue;
 
-                foreach (DungeonGenerator.Room neighbor in rooms.GetNeighbors(room))
-                {
-                    if (pointIsInRectInt(position, neighbor.area)) pointIsIndoor = true;
-                }
+                Transform obj = GameObject.Instantiate(
+                        wallPrefab,
+                        new Vector3(position.x + 0.5f, wallHeight / 2f, position.y + 0.5f),
+                        Quaternion.identity,
+                        parentObject.transform
+                    );
 
-                if (!pointIsIndoor)
-                {
-                    Transform obj = GameObject.Instantiate(
-                            wallPrefab,
-                            new Vector3(position.x + 0.5f, wallHeight / 2f, position.y + 0.5f),
-                            Quaternion.identity,
-                            transform
-                        );
+                obj.localScale = new Vector3(1, wallHeight, 1);
+                obj.name = "Wall_" + position.x + "_" + position.y;
 
-                    obj.localScale = new Vector3(1, wallHeight, 1);
-                }
+                discoveredPositions.Add(position);
 
                 yield return new WaitForSeconds(TimeBetweenSteps);
             }
         }
-    }
-    private bool pointIsInRectInt(Vector2 point, RectInt rectInt)
-    {
-        bool pointIsIn = false;
-
-        bool xIsIn = false;
-        bool yIsIn = false;
-
-        if (point.x >= rectInt.xMin && point.x < rectInt.xMax) xIsIn = true;
-        if (point.y >= rectInt.yMin && point.y < rectInt.yMax) yIsIn = true;
-
-        if (xIsIn && yIsIn) pointIsIn = true;
-
-        return pointIsIn;
     }
 }
